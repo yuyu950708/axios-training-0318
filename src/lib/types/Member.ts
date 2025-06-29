18910901d500028096d01a71e1a1eb214f12ef9c
yuyu950708 <jieyu950708@gmail.com>
2025-06-29 22:47:23 +0800

    移除 Response<T> 類型中的 name 和 email 方法定義

--- src/lib/types/Member.ts
+++ src/lib/types/Member.ts
@@ -1,6 +1,4 @@
 export type Response<T> = {
-    name(arg0: string, name: any): unknown;
-    email(arg0: string, email: any): unknown;
     result: boolean;
     errorCode: string;
     message?: string;
